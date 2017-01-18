23f69a5ca7c6300778cdbc2e10ae4e39d6abb3ad
David Fastovich <fastovich@wisc.edu>
2017-01-17 22:01:04 -0600

    Through questions 9

--- Quiz1.r
+++ Quiz1.r
@@ -3,3 +3,29 @@
 setwd('C:\Users\david\Git\STAT327')
 
 #Q1
+(1005)^(1/4)
+
+#Q2
+1005/19
+
+#Q3
+1005%/%19
+
+#Q4
+1005%%19
+
+#Q5
+log(1005, base = 19)
+
+#Q6
+round(17.250, digits = 0)
+
+#Q7
+floor(10.647)
+
+#Q8
+pnorm(11.8, 12, 6, lower.tail = TRUE)
+
+#Q9
+qnorm(0.3, 12, 6, lower.tail = TRUE)
+
