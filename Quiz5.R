5867499de1cec3b1e58946a8edb62c6e2d68013d
David Fastovich <fastovich@wisc.edu>
2017-01-31 23:09:33 -0600

    Finished - 10/10

---
+++ Quiz5.R
@@ -0,0 +1,26 @@
+#Quiz 5
+
+#1
+?points
+
+#2
+boxplot(mpg ~ gear, data = mtcars)
+
+#3
+boxplot(Petal.Length ~ Species, data = iris)
+
+#4
+hist(iris$Petal.Length, seq(0, 7))
+
+#5
+hist(iris$Petal.Length, freq = FALSE)
+
+#6
+pairs(iris)
+
+#7
+
+
+#8
+counts <- table(chickwts)
+barplot(counts)
