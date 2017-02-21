22341bbf489c3992cda473846ba3b9347a86d379
David Fastovich <fastovich@wisc.edu>
2017-02-21 14:27:57 -0600

    Finished letter grade function

--- Section2/hw1.R
+++ Section2/hw1.R
@@ -60,7 +60,19 @@ stopifnot(sign.string( 3) == "positive")
 #
 # Use "if ... else if ... else".
 letter.grade = function(score) {
-  # ...
+  if (score >= 90 & score <= 100) {
+    return("A")
+  } else if (score >= 80 & score < 90) {
+    return("B")
+  } else if (score >= 70 & score < 80) {
+    return("C")
+  } else if (score >= 60 & score < 70) {
+    return("D")
+  } else if (score >= 0 & score < 60) {
+    return("F")
+  } else {
+    return(NULL)
+  }
 }
 stopifnot(letter.grade(100) == "A")
 stopifnot(letter.grade( 95) == "A")
