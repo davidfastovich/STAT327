e91cd34fb7307be7ab86825fc6f3e0c5cc8ba4b3
David Fastovich <fastovich@wisc.edu>
2017-03-11 21:49:55 -0600

    Actually fixed board - the board is small might want to fix later

--- Section2/hw3/hw3game.R
+++ Section2/hw3/hw3game.R
@@ -5,21 +5,23 @@ rm(list=ls())
 x = rep(1:3, each = 3)
 y = rep(1:3, times = 3)
 
-plot(x, y, type="n", xlim=c(0, 8), ylim=c(6, 0))
+plot(x, y, type="n", xlim=c(0, 8), ylim=c(7, 0))
 segments(x0 = c(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5), 
-         y0 = c(6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
+         y0 = c(6.5, 5.5, 4.5, 3.5, 2.5, 1.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5), 
          x1 = c(7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5), 
-         y1 = c(6, 5, 4, 3, 2, 1, 0, 6, 0, 6, 6, 6, 6, 6, 6, 6))
+         y1 = c(6.5, 5.5, 4.5, 3.5, 2.5, 1.5, 0.5, 6.5, 0.5, 6.5, 6.5, 6.5, 6.5, 6.5, 6.5, 6.5))
 
-
+# Data matrix used to keep track of the game
 board = matrix(data=rep("E", times=42), nrow=6, ncol=7)
 
+# Loop that keeps asking people to pkay
 player = "X"
 for (i in 1:42) {
   cat(sep="", "i=", i, ", player=", player, "\n")
   player = ifelse(test=(player == "X"), yes="O", no="X")
 }
 
+# Click to play loop
 player = "X"
 for (i in 1:42) {
   index = identify(x, y, n=1)
@@ -30,12 +32,5 @@ for (i in 1:42) {
   cat(sep="", "i=", i, ", player=", player, ", index=", index,
       ", row=", row, ", col=", col, ", board:", "\n")
   print(board)
-  # ... player takes a turn ...
   player = ifelse(test=(player == "X"), yes="O", no="X")
 }
-
-plot(x, y, type="n", xlim=c(0, 8), ylim=c(6, 0))
-segments(x0 = c(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5), 
-         y0 = c(6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-         x1 = c(7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5), 
-         y1 = c(6, 5, 4, 3, 2, 1, 0, 6, 0, 6, 6, 6, 6, 6, 6, 6))
