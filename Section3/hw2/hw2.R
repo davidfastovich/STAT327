950d9ec3e4b72daa22cc610a44a3e48785723e1c
David Fastovich <fastovich@wisc.edu>
2017-04-18 13:29:07 -0500

    Initial commit

---
+++ Section3/hw2/hw2.R
@@ -0,0 +1,8 @@
+setwd("~/Git/STAT327/Section3/hw2")
+
+# Loading libraries needed to create package
+library(roxygen2)
+library(devtools)
+
+# Creating robust package
+create("robust")
---
+++ Section3/hw2/robust/R/lad.R
@@ -0,0 +1,14 @@
+#' (title at top of help page)
+#'
+#' (Description paragraph)
+#' @param x (description of x; one line per parameter: Arguments section)
+#' @return (Value section)
+#' @details (Details section)
+#' @export
+#' @examples
+#' # (Examples section)
+#' f(x)
+#'
+lad <- function(x, y) {
+  
+}
