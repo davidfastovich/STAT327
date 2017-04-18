187975fe559c0d96cac6052670e553017a8e5b3b
David Fastovich <fastovich@wisc.edu>
2017-04-18 14:29:51 -0500

    Got sad working, giving wrong values

--- Section3/hw2/robust/R/lad.R
+++ Section3/hw2/robust/R/lad.R
@@ -10,5 +10,21 @@
 #' f(x)
 #'
 lad <- function(x, y) {
+  new <- vector()
+  class(new) <- "lad"
+  sad.function <- function(x, y, b) {
+    b0 <- b[1]
+    b1 <- b[2]
+    value <- y - b0 - (b1*x)
+    return((sum(abs(value))))
+  }
+  par <- lm(y ~ x)
+  optim(par = c(par$coefficients[1],par$coefficients[2]), fn = sad.function, x = area$land, y = area$farm)
   
 }
+
+# Test lm
+x = area$land
+y = area$farm
+lm(farm ~ land, data = area)
+   
