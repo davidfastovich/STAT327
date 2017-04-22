822f7dbe4adf3b8fed9ce8e7d1b22c639fab2416
davidfastovich <fastovich@wisc.edu>
2017-04-22 08:38:41 -0500

    Predict.lad working

--- Section3/hw2/robust/R/predict.R
+++ Section3/hw2/robust/R/predict.R
@@ -9,8 +9,19 @@
 #' # (Examples section)
 #' f(x)
 #'
-predict.lad <- function(x) {
-  value <- x$fitted.value
-  names(value) <- 1:length(x$fitted.values)
-  return(value)
+predict.lad <- function(x,new.x = FALSE) {
+  if (missing(new.x)) {
+    value <- x$fitted.value
+    names(value) <- 1:length(x$fitted.values)
+    return(value)
+    } else {
+    predict.func <- function(new.x) {
+      b <- x$coefficients
+      y.hat <- b[1] + b[2]*new.x
+      names(y.hat) <- 1:length(new.x)
+      return(y.hat)
+      }
+    new.y.hat <- predict.func(new.x)
+      return(new.y.hat)
+    }
 }
