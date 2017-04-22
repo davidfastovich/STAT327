4ecb49c783f37c8a07252c58be46304cde7259b7
davidfastovich <fastovich@wisc.edu>
2017-04-21 22:12:41 -0500

    Area data script added

---
+++ Section3/hw2/robust/R/area.R
@@ -0,0 +1,12 @@
+#' (title at top of help page)
+#'
+#' (Description paragraph)
+#' @param x (description of x; one line per parameter: Arguments section)
+#' @return (Value section)
+#' @details (Details section)
+#' @export
+#' @examples
+#' # (Examples section)
+#' f(x)
+#'
+read.csv("http://www.stat.wisc.edu/~jgillett/327-3/2/farmLandArea.csv")
