21184d97f648c89660350fca3f85bc8565bb01d6
davidfastovich <fastovich@wisc.edu>
2017-04-23 19:05:23 -0500

    Added description for print

--- Section3/hw2/robust/R/print.R
+++ Section3/hw2/robust/R/print.R
@@ -1,13 +1,12 @@
-#' (title at top of help page)
+#' Print LAD object coefficients
 #'
-#' (Description paragraph)
-#' @param x (description of x; one line per parameter: Arguments section)
-#' @return (Value section)
-#' @details (Details section)
+#' This is a general function to print the regression coefficients from the \code{lad} function.
+#' @param x An object from the \code{lad} function.
+#' @return Regression coefficients to the console.
 #' @export
 #' @examples
-#' # (Examples section)
-#' f(x)
+#' results <- lad(x = area$land, y = area$farm)
+#' print(results)
 #'
 print.lad <- function(x) {
   cat(sep = "", "Intercet", "\n",x$coefficient[1], "\n", "\n", "Slope", "\n", x$coefficient[2])
